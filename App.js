5c74f62d0d01ab903435c014274f81b5b1f43b4c
javitocor <56809963+javitocor@users.noreply.github.com>
2021-08-20 08:12:22 +0200

    add main component

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage, Text, View } from 'react-native';
+import SocketIOClient from 'socket.io-client';
+import {GiftedChat} from 'react-native-gifted-chat';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const USER_ID = '@userId';
+
+class Main extends React.Component {
+  constructor(props) {
+    this.state= {
+      messages: [],
+      userId: null,
+    }
+    this.determineUser = this.determineUser.bind(this);
+    this.onReceivedMessage = this.onReceivedMessage.bind(this);
+    this.onSend = this.onSend.bind(this);
+    this._storeMessages = this._storeMessages.bind(this);
+
+    this.socket = SocketIOClient('http://localhost:3000');
+    this.socket.onReceivedMessage('message', this.onReceivedMessage)
+    this.determineUser();
+  }
+
+  determineUser(){
+    AsyncStorage.getItem(USER_ID)
+    .then((userId)=>{
+      if(!userId) {
+        this.socket.emit('userJoined', null)
+        this.socket.on('userJoined', (userId) => {
+          AsyncStorage.setItem(USER_ID, userId)
+          this.setState({userId});
+        })
+      } else {
+        this.socket.emit('userJoined', userId);
+        this.setState({userId});
+      }      
+    })
+    .catch(err => alert(err));
+  }
+  onReceivedMessage(messages){
+    this._storeMessages(messages);
+  }
+  onSend(messages=[]){
+    this.socket.emit('messages', messages[0]);
+    this._storeMessages(messages);
+  }
+
+  render () {
+    var user = {_id: this.state.userId || -1}
+
+    return (
+      <GiftedChat
+      messages= {this.state.messages}
+      onSend={this.onSend}
+      user={user}
+      />
+    );
+  }
+
+  _storeMessages(messages){
+    this.setState((previousState) =>{
+      return {
+        messages: GiftedChat.append(previousState.messages, messages);
+      }
+    })
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+module.exports = Main;
